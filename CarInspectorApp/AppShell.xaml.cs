0d8e754b497c17044cc4134331907b84cafd12c3
Peter Vanhorn <petervanhornmt@gmail.com>
2024-07-01 17:56:20 -0600

    added project to repository

---
+++ CarInspectorApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CarInspectorApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CarInspectorApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CarInspectorApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
